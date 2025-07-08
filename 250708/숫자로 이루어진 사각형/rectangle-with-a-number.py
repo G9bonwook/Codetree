023af6bcf1023e9b560d4ae5c216452651f8c1a2
G9bonwook <145426357+G9bonwook@users.noreply.github.com>
2025-07-08 21:22:15 +0900

    Title: 숫자로 이루어진 사각형, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250708/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+def square(n):
+    cnt = 1
+    for i in range(n):
+        for j in range(n):
+            print(cnt,end=" ")
+            if cnt == 9:
+                cnt = 1
+            else:
+                cnt += 1
+        print()
+        
+square(4)
